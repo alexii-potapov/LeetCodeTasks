5a5786229c79e4ac13f679d190bf43789359bad2
Aleksey Potapov <alexii.potapov@gmail.com>
2024-01-17 23:09:08 +0300

    Task 189

---
+++ LeetCodeApp/Solutions/Easy/Solution189.cs
@@ -0,0 +1,49 @@
+﻿
+public class Solution189
+{
+    public static void Rotate(int[] nums, int k)
+    {
+        var dif = k % nums.Length;
+        if (k == 0 || dif == 0)
+        {
+            return;
+        }
+
+        var g = GCD(nums.Length, dif);
+        if (g == 1)
+        {
+            Shift(nums, dif, 0, nums.Length);
+        }
+        else
+        {
+            for (int i = 0; i < g; i++)
+            {
+                Shift(nums, dif, i, nums.Length / g);
+            }
+        }
+
+    }
+    private static int GCD(int x, int y)
+    {
+        return y == 0 ? x : GCD(y, x % y);
+    }
+
+    private static void Shift(int[] nums, int k, int startIndex, int length)
+    {
+        var lastValue = nums[k];
+        var currentIndex = startIndex;
+        int nextIndex, nextValue;
+
+        for (int i = 0; i < length; i++)
+        {
+            nextValue = nums[currentIndex];
+            nums[currentIndex] = lastValue;
+
+            nextIndex = (currentIndex + k) % nums.Length;
+            lastValue = nextValue;
+            currentIndex = nextIndex;
+        }
+
+        nums[startIndex] = lastValue;
+    }
+}
---
+++ TestProject/Solutions/Easy/Solution189Tests.cs
@@ -0,0 +1,89 @@
+namespace TestProject.Solutions.Easy;
+
+public class Solution189Tests
+{
+    [Test]
+    public void SeveralElementsTest()
+    {
+        var nums = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+
+        Solution189.Rotate(nums, 3);
+
+        var result = string.Join(',', nums);
+
+        Assert.That(result, Is.EqualTo("5,6,7,1,2,3,4"));
+    }
+
+    [Test]
+    public void SeveralElementsTest2()
+    {
+        var nums = new int[] { -1, -100, 3, 99 };
+
+        Solution189.Rotate(nums, 2);
+
+        var result = string.Join(',', nums);
+
+        Assert.That(result, Is.EqualTo("3,99,-1,-100"));
+    }
+
+
+    [Test]
+    public void SeveralElementsTest3()
+    {
+        var nums = new int[] { 1, 2, 3, 4, 5, 6 };
+
+        Solution189.Rotate(nums, 3);
+
+        var result = string.Join(',', nums);
+
+        Assert.That(result, Is.EqualTo("4,5,6,1,2,3"));
+    }
+
+    [Test]
+    public void SeveralElementsTest4()
+    {
+        var nums = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54 };
+
+        Solution189.Rotate(nums, 45);
+
+        var result = string.Join(',', nums);
+
+        Assert.That(result, Is.EqualTo("10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,1,2,3,4,5,6,7,8,9"));
+    }
+
+    [Test]
+    public void SeveralElementsTest5()
+    {
+        var nums = new int[] { 1, 2 };
+
+        Solution189.Rotate(nums, 3);
+
+        var result = string.Join(',', nums);
+
+        Assert.That(result, Is.EqualTo("2,1"));
+    }
+
+    [Test]
+    public void SeveralElementsTest6()
+    {
+        var nums = new int[] { 1, 2, 3, 4, 5, 6 };
+
+        Solution189.Rotate(nums, 2);
+
+        var result = string.Join(',', nums);
+
+        Assert.That(result, Is.EqualTo("5,6,1,2,3,4"));
+    }
+
+    [Test]
+    public void SeveralElementsTest7()
+    {
+        var nums = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+
+        Solution189.Rotate(nums, 6);
+
+        var result = string.Join(',', nums);
+
+        Assert.That(result, Is.EqualTo("15,16,17,18,19,20,1,2,3,4,5,6,7,8,9,10,11,12,13,14"));
+    }
+}
