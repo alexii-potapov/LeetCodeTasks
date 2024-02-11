2a25f061dc076f7a74aa0da41a7480dfd40ced66
Aleksey Potapov <alexii.potapov@gmail.com>
2024-02-11 21:41:57 +0300

    Task 392.

---
+++ LeetCodeApp/Solutions/String/Solution392.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCodeApp.Solutions.String;
+
+public class Solution392
+{
+    public static bool IsSubsequence(string s, string t)
+    {
+        if (s.Length == 0)
+        {
+            return true;
+        }
+
+        int indexS = 0;
+        for (int i = 0; i < t.Length; i++)
+        {
+            if (t[i] == s[indexS])
+            {
+                indexS++;
+                if (indexS == s.Length)
+                {
+                    return true;
+                }
+            }
+        }
+
+        return indexS == s.Length;
+
+    }
+}
